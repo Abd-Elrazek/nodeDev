411a6a5916db0c3a2b7e4027ebc567681ee69811
thekikao <me@thekikao.com>
2017-08-22 21:25:19 +0200

    . initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+//@EVO_TODO: use ritalin next time!!
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// register middleware
+app.use((req, res, next) => {
+    let log = `${new Date().toString()}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.warning('!! unable to append to \'server.log\'');
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+// helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (txt) => {
+    return txt.toUpperCase();
+});
+hbs.registerHelper('generateMenu', () => {
+    return pages;
+});
+
+
+var pages = {
+    home: '/',
+    about: '/about',
+    bad: '/bad'
+};
+
+// root page
+app.get(pages.home, (req, res) => {
+    res.render('home.hbs', {
+        title: 'welcome to <u>batmans</u> page',
+        text: 'crazy stuff here, hu?'
+    });
+});
+
+// about page
+app.get(pages.about, (req, res) => {
+    res.render('about.hbs', {
+        title: 'awesome about',
+        text: 'crazy text here, hu?'
+    });
+});
+
+// error handling page
+app.get(pages.bad, (req, res) => {
+    res.send({
+        errorMsg: 'wrong page, fucker!'
+    });
+});
+
+
+// start server
+let port = 3333;
+app.listen(port, () => {
+    console.log('## server is up on port', port);
+});
